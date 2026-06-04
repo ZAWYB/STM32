6144a8b99fb84793ed3e7ad34ac1c822f249592b
286978640@qq.com <286978640@qq.com>
2026-06-04 10:01:33 +0800

    OLED库建立完成

--- STM32H743XI/OLED_TEST/Core/Src/main.c
+++ STM32H743XI/OLED_TEST/Core/Src/main.c
@@ -23,7 +23,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "oled.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -93,6 +93,37 @@ int main(void)
   MX_GPIO_Init();
   MX_I2C1_Init();
   /* USER CODE BEGIN 2 */
+  /* USER CODE BEGIN 2 */
+  OLED_Init();                           //OLED初始
+  OLED_Clear();                         //清屏
+  //上面的初始化以及清屏的代码在一开始处一定要写
+  OLED_ShowString(0,0,"UNICORN_LI",16, 1);    //反相显示8X16字符串
+  OLED_ShowString(0,2,"unicorn_li_123",12,0);//正相显示6X8字符串
+
+  OLED_ShowCHinese(0,4,0,1); //反相显示汉字“独”
+  OLED_ShowCHinese(16,4,1,1);//反相显示汉字“角”
+  OLED_ShowCHinese(32,4,2,1);//反相显示汉字“兽”
+  OLED_ShowCHinese(0,6,0,0); //正相显示汉字“独”
+  OLED_ShowCHinese(16,6,1,0);//正相显示汉字“角”
+  OLED_ShowCHinese(32,6,2,0);//正相显示汉字“兽”
+
+  OLED_ShowNum(48,4,6,1,16, 0);//正相显示1位8X16数字“6”
+  OLED_ShowNum(48,7,77,2,12, 1);//反相显示2位6X8数字“77”
+  OLED_DrawBMP(90,0,122, 4,BMP1,0);//正相显示图片BMP1
+  OLED_DrawBMP(90,4,122, 8,BMP1,1);//反相显示图片BMP1
+
+  OLED_HorizontalShift(0x26);//全屏水平向右滚动播放
+
+  //显示正负浮点数的代码
+  //  float num1=-231.24;
+  //  float num2=23.375;
+
+  //OLED_ShowString(0,0,"Show Decimal",12,0);
+  //OLED_Showdecimal(0,4,num1,3,2,12, 0);
+  //OLED_Showdecimal(0,6,num2,2,3,16, 1);
+
+
+  /* USER CODE END 2 */
 
   /* USER CODE END 2 */
 
@@ -103,6 +134,7 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+
   }
   /* USER CODE END 3 */
 }
